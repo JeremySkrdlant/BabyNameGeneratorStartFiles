e37070a99915977894a42d21f132ab4f006badcd
JeremySkrdlant <jeremy.skrdlant@nwktc.edu>
2015-01-11 16:41:52 -0700

    Initial Commit
    HTML Page, Unit Test Page, Javascript, and CSS

---
+++ OhBaby.js
@@ -0,0 +1,7 @@
+// Add the code to set up a click event on the submit div here
+
+function generateName(nameLength, percentageVowels)
+{
+    //Put your own code in here to get all the unit tests to pass.
+    return "This Name is Invalid";
+}
